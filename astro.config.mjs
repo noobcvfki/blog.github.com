d236734b81d34c004918de2ab9ff1c5970c3962c
capting <2364448632@qq.com>
2025-06-07 18:13:05 +0800

    refactor: 修改网站 URL 为 caption.github.com
    - 将 astro 配置中的 site URL 从 blog.github.com 更改为 caption.github.com
    - 这个更改影响网站的基本配置，确保所有内部链接和 SEO 设置都指向新的域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.github.com",
+	site: "https://caption.github.com",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
