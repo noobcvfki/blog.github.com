b8811a91d587b1e065bc1cb30db477eeca085e29
capting <2364448632@qq.com>
2025-06-07 18:22:23 +0800

    refactor(astro): 更新网站配置
    - 修改网站域名从 "https://caption.github.com" 到 "https://noobcvfki.github.com"
    - 更新网站基础路径从 "/" 到 "/blog.github.com"

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://caption.github.com",
-	base: "/",
+	site: "https://noobcvfki.github.com",
+	base: "/blog.github.com",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
