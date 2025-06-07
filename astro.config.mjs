f108d0af00094eb606fd1fc7ba4454639fdaa5e4
capting <2364448632@qq.com>
2025-06-07 18:11:31 +0800

    docs: 更新网站配置
    - 将 site URL 从 "https://captain.github.io/" 修改为 "https://blog.github.com"
    - 设置 base 路径为 "/"

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://captain.github.io/",
+	site: "https://blog.github.com",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
